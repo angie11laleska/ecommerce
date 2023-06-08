27891633f4973b9c38b5e323f330ae9f4e9a68bf
Madeling Cabrera <madecr28@gmail.com>
2023-06-08 16:16:55 -0600

    se trabajo en el registro

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, flash, redirect, render_template, request, session
-
+from .models import *
 
 app = Flask(__name__)
 
@@ -28,3 +28,41 @@ def trending():
 @app.route("/registrarse", methods=["GET", "POST"])
 def registrarse():
     return render_template("registrarse.html")
+
+app = Flask(__name__)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    nombre = request.form.get("nombre")
+    apellido = request.form.get("apellido")
+    contraseña = request.form.get("contraseña")
+    confirmacion = request.form.get("confirmacion")
+    direccion = request.form.get("direccion")
+    departamento = request.form.get("departamento")
+    numTelefono = request.form.get("celular")
+    if not nombre:
+        return redirect("/login")
+    if not apellido:
+        return redirect("/login")
+    if not contraseña:
+        return redirect("/login")
+    if not confirmacion:
+        return redirect("/login")
+    if not direccion:
+        return redirect("/login")
+    if not departamento:
+        return redirect("/login")
+    if contraseña != confirmacion:
+        return redirect("/login")
+    try:
+        query = text("INSERT INTO persona(nombre_persona, apellido, direccion, celular, pago, contra, roles) VALUES (:nombre, :direccion, :celular, :pago, :contra, :roles)")
+        db.execute(query, {"nombre":nombre, "contra": contraseña, "direccion": direccion, "celular": numTelefono, "pago": 0, "roles": 0})
+        db.commit
+    except:
+        return redirect("/login")
+    return redirect("/login")
+
